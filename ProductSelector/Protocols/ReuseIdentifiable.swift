f8a24ac5c847e8321e550ad170b74e2476309705
nickwatson5 <53030877+nickwatson5@users.noreply.github.com>
2021-11-26 09:33:59 +0000

    Add files via upload

---
+++ ProductSelector/Protocols/ReuseIdentifiable.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol ReuseIdentifiable {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReuseIdentifiable {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
